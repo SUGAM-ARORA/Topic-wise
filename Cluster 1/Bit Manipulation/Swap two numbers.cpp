c7db9c5b2e1a3698f53adccd43ef60c24528d63b
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-04-22 00:35:37 +0530

    Create Swap two numbers.cpp

---
+++ Cluster 1/Bit Manipulation/Swap two numbers.cpp
@@ -0,0 +1,11 @@
+##GFG
+
+//User function Template for C++
+
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+        //complete the function here
+        return{b,a};
+    }
+};
