3da9498c45ea2746156adacdbe7a3d1ca75df20d
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-04-22 00:38:47 +0530

    Create Division without using multiplication, division and mod operator.cpp

---
+++ Cluster 1/Bit Manipulation/Division without using multiplication, division and mod operator.cpp
@@ -0,0 +1,13 @@
+##GFG
+
+//User function Template for C++
+
+class Solution
+{
+    public:
+    long long divide(long long dividend, long long divisor) 
+    {
+        //code here
+        return(dividend/divisor);
+    }
+};
