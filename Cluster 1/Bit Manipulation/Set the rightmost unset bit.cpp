bdae7c5384bac588d5c846f77b2d5b024a211182
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-04-22 00:33:19 +0530

    Create Set the rightmost unset bit.cpp

---
+++ Cluster 1/Bit Manipulation/Set the rightmost unset bit.cpp
@@ -0,0 +1,17 @@
+##GFG
+
+
+class Solution
+{
+public:
+    int setBit(int N)
+    {
+        // Write Your Code here
+        if(N & N+1){
+            return (N | N+1);
+        }
+        else{
+            return N;
+        }
+    }
+};
