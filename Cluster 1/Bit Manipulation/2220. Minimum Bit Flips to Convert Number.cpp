80560afeb16a33a7706287cd07d875cf208fbb05
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-04-22 14:00:11 +0530

    Create 2220. Minimum Bit Flips to Convert Number.cpp

---
+++ Cluster 1/Bit Manipulation/2220. Minimum Bit Flips to Convert Number.cpp
@@ -0,0 +1,14 @@
+##Leetcode
+
+class Solution {
+public:
+     int minBitFlips(int start, int goal) {
+        int value = start^goal , count=0;
+        while(value)
+        {
+            value&=value-1;
+            count++;
+        }
+        return count;
+    }
+};
